f7d967859309f0d687992829775a4e742c1561f9
dev-palwar <devpalwar06@gmail.com>
2024-11-15 20:14:27 +0530

    initial commit with basic functionalities

---
+++ src/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String title;
+    private boolean isCompleted;
+
+    public Task(String title, boolean isCompleted) {
+        this.title = title;
+        this.isCompleted = isCompleted;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void markCompleted() {
+        isCompleted = true;
+    }
+
+    @Override
+    public String toString() {
+        return title + " (Completed: " + (isCompleted ? "Yes" : "No") + ")";
+    }
+}
---
+++ src/TaskManager.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private List<Task> tasks;
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+    public List<Task> getPendingTasks() {
+        ArrayList<Task> pendingTasks = new ArrayList<>();
+
+        for (Task task : tasks) {
+            if (!task.isCompleted()) {
+                pendingTasks.add(task);
+            }
+        }
+
+        return pendingTasks;
+    }
+
+    public void markCompleted(int index) {
+        for (int i = 0; i < tasks.size(); i++) {
+            if (i == index && !tasks.get(i).isCompleted()) {
+                tasks.get(i).markCompleted();
+            } else {
+                System.out.println("Task already completed!");
+            }
+        }
+    }
+}
